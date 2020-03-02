857bd334507f703e08ff3e210147cda978a24330
JJohnson45 <Jd.johnson452@gmail.com>
2020-03-02 16:47:48 -0600

    somwthing

--- server/index.js
+++ server/index.js
@@ -3,11 +3,21 @@ const parser = require('body-parser');
 const path = require('path');
 const { apiRouter } = require('./api/index');
 const { spotifyRouter } = require('./auth-server/authorization_code/app');
-
+const csv = require('csv-parser');
+const fs = require('fs');
 const app = express();
 
 // app.set('views', path.join(__dirname, 'views'));
 // app.engine('html', require('ejs').renderFile);
+// let all = fs.createReadStream('../regional-global-daily-latest.csv')
+//   .pipe(csv())
+//   .on('data', (row) => {
+//     console.log(row);
+//   })
+//   .on('end', () => {
+//     console.log('CSV file successfully processed');
+//   });
+
 
 // app.set('view engine', 'html');
 
